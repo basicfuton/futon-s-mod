e7e28e9de3b158bbea38a267ec58dcbdc5e01ca0
basicfuton <macmatt2003@gmail.com>
2023-12-17 15:52:39 -0800

    test

--- src/main/java/ca/basicfuton/futonsmod/Main.java
+++ src/main/java/ca/basicfuton/futonsmod/Main.java
@@ -1,13 +1,69 @@
 package ca.basicfuton.futonsmod;
 
-public class Main {
-    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+import com.mojang.logging.LogUtils;
 
-    public static final RegistryObject<Block> ROCK_BLOCK = BLOCKS.register("rock",
-            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE))
-    );
+import ca.basicfuton.futonsmod.item.ModItems;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.core.registries.Registries;
+//import net.minecraft.world.food.FoodProperties;
+//import net.minecraft.world.item.BlockItem;
+//import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+//import net.minecraft.world.item.Item;
+//import net.minecraft.world.level.block.Block;
+//import net.minecraft.world.level.block.Blocks;
+//import net.minecraft.world.level.block.state.BlockBehaviour;
+//import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+//import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+//import net.minecraftforge.registries.DeferredRegister;
+//import net.minecraftforge.registries.ForgeRegistries;
+//import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(Main.MOD_ID)
+public class Main {
+    public static final String MOD_ID = "futonsmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public Main() {
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.CAT_ITEM);
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
     }
 }
