0721b813a2d7220d31e71e4cf7a4578fa9855b6a
basicfuton <macmatt2003@gmail.com>
2023-12-17 12:48:00 -0800

    test update

--- src/main/java/ca/basicfuton/futonsmod/Main.java
+++ src/main/java/ca/basicfuton/futonsmod/Main.java
@@ -1,5 +1,13 @@
 package ca.basicfuton.futonsmod;
 
 public class Main {
-    
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+    public static final RegistryObject<Block> ROCK_BLOCK = BLOCKS.register("rock",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE))
+    );
+
+    public Main() {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
 }
